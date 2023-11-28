296d6518c3b22050b00057cb7aed06599880b203
mavie <mave@yahoo.com>
2023-11-28 15:07:33 -0800

    p

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for tens_digit in range(10):
-    for ones_digit in range(tens_digit + 1, 10):
+for tens_digit in range(1, 10):
+    for ones_digit in range(tens_digit, 10):
         print(f"{tens_digit}{ones_digit}", end=", " if ones_digit < 9 else "\n")
