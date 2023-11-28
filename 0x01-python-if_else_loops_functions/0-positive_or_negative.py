a3f4dd92a58d7549cb0d574186c4ed66eec1511a
mavie <mave@yahoo.com>
2023-11-28 13:24:44 -0800

    p

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-if number > 0:
-print(number,' is positive')
+if number >0:
+    print("{:d} is positive".format(number))
 
-elif number == 0:
-print(number, 'is Zero')
+elif number < 0:
+    print("{:d} is negative".format(number))
 
-else number < 0:
-print(number , 'is negative')
+else :
+    print("{:d} is zero".format(number))
 
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
-    if i !=  ['q', 'e']:
-        print("{:c}".format(i), end='')
+    if i !=  ord('q'), ord('e'):
+        print("{:c}".format(i), end="")
