80483ae3c74b887ebe0c76bdd7c8ad7cab331f40
mavie <mave@yahoo.com>
2023-11-30 09:35:27 -0800

    P

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
 from add_0 import add 
-def main():
-    a = 1
-    b = 2
-    
-    res = add(a,b)
-    print(res)
-    main()
+     a = 1
+     b = 2
+     result = add(a, b)
+     print("{} + {} = {}".format(a, b, result))
