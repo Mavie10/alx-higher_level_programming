d93fb347a4846a7060d409024259a693aae8bdb3
mavie <mave@yahoo.com>
2023-12-04 04:46:03 -0800

    p

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,5 +2,6 @@
 def max_integer(my_list=[]):
     if my_list = [] :
         return None
-    my_list.sort
-    return mylist[-1]
+    list_copy = my_list.copy()
+    list_copy.sort()
+    return list_copy[-1]
