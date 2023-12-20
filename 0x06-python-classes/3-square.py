34320afc5901c6750ef6cbe3cce1652ab79772c6
mavie <mave@yahoo.com>
2023-12-19 18:33:21 -0800

    OOP

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+"""Square"""
+
+
 class Square:
+    """Define the Square"""
+
     def __init__(self, size=0):
+        """Constructor: Set the size of the square."""
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
-        sel.__size = size
-        def area(self):
-            return self.__size ** 2
+        self.__size = size  # Corrected the variable name
+
+    def area(self):
+        """Calculate the area of the square."""
+        return self.__size ** 2
