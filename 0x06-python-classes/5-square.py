3856493c46e9e688314242cf9624c9bc43cecf6b
mavie <mave@yahoo.com>
2023-12-19 18:42:33 -0800

    OOP

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python3
+"""Square"""
+
+
 class Square:
-def __init__(self, size=0):
-        self.size - size
+    """Define square"""
 
-        @property
-def size(self):
-            return self.__size
+    def __init__(self, size=0):
+        """Constructor: Set the size of the square."""
+        self.size = size
 
-        @size.setter
-def size(self, value):
-    if not isinstance(value, int):
-        raise TypeError('size must be an integer')
-    if size < 0: 
-        raise ValueError('size must be >= 0')
-    self.__size = value
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.__size
 
-def area(self):
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """Calculate the area of the square."""
         return self.__size ** 2
 
-def my_print(self):
-    if i in range(self.size):
-        for j in range(self.size):
-            print("#", end="\n" if j is self.size -1 and i != j else "")
-    print()
+    def my_print(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#", end="\n" if j == self.size - 1 and i != j else "")
+        print()
+
+
