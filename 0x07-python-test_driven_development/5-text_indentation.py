778108257d4be86b9caf7cc9cc1e938e7ece914a
mavie <mave@yahoo.com>
2024-01-04 05:12:52 -0800

    testing

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module text_indentation method"""
+
+def text_indentation(text):
+    """method for adding 2 new lines after each of these characters: '., ?:' chars.
+
+    args:
+        text: the str text
+
+    raises:
+        TypeError: if text is not a str
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+    for delim in ".,?:":
+        #print(delim, text.split(delim))
+        text = (delim + "\n\n").joiin(
+            [line.strip(" ") for line in text.split(delim)])
+
+        print(text, end="")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
