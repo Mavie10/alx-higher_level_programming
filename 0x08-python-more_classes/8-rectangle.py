f1f20628aea1eda73fc604331abb5c36ebc3c429
mavie <mave@yahoo.com>
2024-01-04 16:24:49 -0800

    m

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Define a class rectangle
+Define a class Rectangle
 """
 
 class Rectangle:
@@ -27,7 +27,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Setter for private instance attribute width"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -41,7 +41,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Setter for the private instance attribute height"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
