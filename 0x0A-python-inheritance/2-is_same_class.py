f897ae29eb0ec07238ef56ad6a70a1c6490b78f5
mavie <mave@yahoo.com>
2024-01-08 17:02:59 -0800

    p

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """module for same class method"""
 
+
 def is_same_class(obj, a_class):
     """determine if an object is exact an isntance of a class or no"""
     return type(obj) == a_class
